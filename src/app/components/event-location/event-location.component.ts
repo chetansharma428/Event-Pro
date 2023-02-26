c8536cea8709bbf62280e0ab94a6b911d1b10481
Chetan Sharma <106821304+chetansharma428@users.noreply.github.com>
2023-02-26 20:40:06 +0530

    location Added

--- src/app/components/event-location/event-location.component.ts
+++ src/app/components/event-location/event-location.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-
+import { DOCUMENT } from '@angular/common';
+import { Inject } from '@angular/core';
 @Component({
   selector: 'app-event-location',
   templateUrl: './event-location.component.html',
@@ -7,9 +8,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EventLocationComponent implements OnInit {
 
-  constructor() { }
+  constructor(@Inject(DOCUMENT) private document: Document) { }
 
   ngOnInit(): void {
   }
-
+  gotohotel() {
+    this.document.location.href = '//api.whatsapp.com/send?phone=917001854501&text=Hey! I am coming from the website. I would like to check some information and register for the event in Guwahati which was on 15 july.';
+  }
 }
